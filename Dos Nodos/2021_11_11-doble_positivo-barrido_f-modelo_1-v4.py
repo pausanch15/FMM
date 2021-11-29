cc0b74415ab11134596d1f24a7817694848b8289
pausanch15 <paulijsanchez@live.com.ar>
2021-11-29 16:27:18 -0300

    Pruebo mas cosas

--- Dos Nodos/2021_11_11-doble_positivo-barrido_f-modelo_1-v4.py
+++ Dos Nodos/2021_11_11-doble_positivo-barrido_f-modelo_1-v4.py
@@ -6,6 +6,9 @@ import numpy as np
 import mide_biestabilidad as mb
 import runge_kuta as rk
 import going_up_comming_down_doble_positivo_modelo_1 as gucd
+import latin_hypercube_sampling as lhs
+from time import time
+import pandas as pd
 plt.ion()
 
 #%%
@@ -23,3 +26,47 @@ plt.show()
 #%%
 #Pruebo la función que mide el área biestable
 area = mb.mide_biestabilidad(A_s, S)
+
+#%%
+#Pruebo generar parámetros con el lhs
+#Me fijo cuánto tarda en generar diez sets de parámetros y hacer todo el análisis
+n_parametros = 8
+n_barrido = 10
+parametros = lhs.lhs(n_parametros, n_barrido)
+
+areas = []
+
+ti = time()
+for params in parametros:
+    A_s, B_s, S = gucd.gucd_modelo_1(*params, 1000, 2, 50)
+    area = mb.mide_biestabilidad(A_s, S)
+    if area != None:
+        if area > 0:
+            areas.append(area)
+    del(A_s, B_s, S, area)
+tf = time()
+print(f'Tarda {tf -ti} segundos en hacer todo el análisis para 10 sets de parámetros.')
+
+#%%
+#Hago lo mismo pero ahora con el dataframe en el que voy a ir guardando los parámetros que den biestabilidad 
+df = pd.DataFrame(columns=['K_sa', 'K_sb', 'k_ba', 'k_ab', 'K_ba', 'K_ab', 'k_sa', 'k_sb'])
+
+n_parametros = 8
+n_barrido = 10
+parametros = lhs.lhs(n_parametros, n_barrido)
+
+areas = []
+
+ti = time()
+for params in parametros:
+    A_s, B_s, S = gucd.gucd_modelo_1(*params, 1000, 2, 50)
+    area = mb.mide_biestabilidad(A_s, S)
+    if area != None:
+        if area > 0:
+            df1 = pd.DataFrame(columns=['K_sa', 'K_sb', 'k_ba', 'k_ab', 'K_ba', 'K_ab', 'k_sa', 'k_sb'], index=[str(area)])
+            df1.loc[str(area), :] = params
+            df = df.append(df1)
+            del(df1)
+    del(A_s, B_s, S, area)
+tf = time()
+print(f'Tarda {tf -ti} segundos en hacer todo el análisis para 10 sets de parámetros.')
