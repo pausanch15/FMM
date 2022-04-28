ce0497d79eb073f438ebe09a2b64416e6a1ef4ce
pau <paula@sea.dolph>
2022-04-27 23:16:47 -0300

    Arreglo el escalon

--- Tres Nodos/escalon.py
+++ Tres Nodos/escalon.py
@@ -8,9 +8,9 @@ plt.ion()
 
 #%%
 #Eje del tiempo
-resolucion = 1000
+resolucion = 10000
 ti = 0
-tf = 100
+tf = 500
 tiempo = np.linspace(ti, tf, resolucion)
 
 #Eje de estímulo
@@ -18,30 +18,51 @@ S_min = 0
 S_max = 5
 S = np.ones(resolucion)
 
-#Hago el escalón
-ts = 30
-tb = 70
+#Hago el escalon
+ts = int(0.3*resolucion)
+tb = int(0.7*resolucion)
 S[0:ts] = S_min
 S[ts:tb] = S_max
 S[tb:] = S_min
 
 #Ahora la bajada exponencial
 tau = 10
-bajada = S_max*np.exp(-tiempo[tb:]/tau)
-S[tb:] = bajada
-
-# caida = (S_max/(np.exp(-tb)-np.exp(-tf))) * (np.exp(-tiempo)-np.exp(-tf))
-# S[tb:] = caida[tb:]
-
-# tau = 10
-# bajada = S_max*np.exp(tb/tau)*np.exp(-tiempo/tau)
-# S[tb:] = bajada[tb:]
-
-# tau = 10
-# bajada = S_max*np.exp(-tiempo/tau)
-# S[tb:] = bajada[tb:]
+bajada = S_max*np.exp((-tiempo)/tau)
+S[tb:] = bajada[:resolucion-tb]
 
 #Ploteo
 plt.plot(tiempo, S)
-# plt.plot(tiempo, caida)
 plt.show()
+
+#%%
+#Armo la función que devuelve el escalon
+def escalon(resolucion, ti, tf, S_min, S_max, tau, ts, tb):
+    #Redefino
+    ts = ts*0.1
+    tb = tb*0.1
+    
+    #Eje del tiempo
+    resolucion = 10000
+    ti = 0
+    tf = 500
+    tiempo = np.linspace(ti, tf, resolucion)
+    
+    #Eje de estímulo
+    S_min = 0
+    S_max = 5
+    S = np.ones(resolucion)
+    
+    #Hago el escalon
+    ts = int(0.3*resolucion)
+    tb = int(0.7*resolucion)
+    S[0:ts] = S_min
+    S[ts:tb] = S_max
+    S[tb:] = S_min
+    
+    #Ahora la bajada exponencial
+    tau = 10
+    bajada = S_max*np.exp((-tiempo)/tau)
+    S[tb:] = bajada[:resolucion-tb]
+
+    #Devuelvo el escalon
+    return S
