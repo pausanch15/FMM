9bc1f43abfd0348e90b7da2155a66e89a6603fbe
pau <paula@sea.dolph>
2022-05-27 12:11:16 -0300

    Cuento los picos

--- Tres Nodos/2022_04_25-FPN-con_estimulo-v5.py
+++ Tres Nodos/2022_04_25-FPN-con_estimulo-v5.py
@@ -1,5 +1,5 @@
 #Para casos en donde no vemos reververaciones, hacemos un barrido en los parámetros de feedback positivo F y negativo dYX2.
-#Aplicamostambién el contadorde memoriaque propone el paper para estos casos.
+#Aplicamos también el contadorde memoria que propone el paper para estos casos.
 
 #%%
 #Librerías
@@ -78,6 +78,8 @@ dY2 = 0.1
 epsilon = 0.01
 tb = 530
 
+memoria_f = []
+
 for f in f_s:
     tiempo, variables, tiempo_estimulo, estimulo = fpndesf.integra_FPN_estimulo(dYX2, dX2, Ty2, dy2, TY2, dY2, f, tiempo_max=2000, resolucion=10000, ti=0, tf=2000, S_min=0, S_max=0.04, tau=200, ts=30, tb=530, condiciones_iniciales=[0.01, 0.01, 0.01])
 
@@ -86,27 +88,41 @@ for f in f_s:
     picos, caracteristicas = find_peaks(X[i_tb:], height=X[i_tb])
     altura_picos = caracteristicas['peak_heights']
 
+    #Memoria: la defino como el número de picos que se cuentan una vez que el estímulo aplicado empieza a decrecer
+    memoria_f.append(len(picos))
+
     #Plots
-    plt.figure()
-    plt.plot(tiempo, X, label='X')
-    plt.plot(tiempo[i_tb:][picos], altura_picos, 'o', label='Memoria')
-    plt.plot(tiempo_estimulo, estimulo, 'k', label='Estímulo')
-    plt.grid()
-    plt.title(f'{f=:.2}')
-    plt.legend()
-    plt.show()
-    plt.savefig(f'2022_05_26-memoria-barrido_f_{f}.png')
-    plt.close()
+    # plt.figure()
+    # plt.plot(tiempo, X, label='X')
+    # plt.plot(tiempo[i_tb:][picos], altura_picos, 'o', label='Memoria')
+    # plt.plot(tiempo_estimulo, estimulo, 'k', label='Estímulo')
+    # plt.grid()
+    # plt.title(f'{f=:.2}')
+    # plt.legend()
+    # plt.show()
+    # plt.savefig(f'2022_05_26-memoria-barrido_f_{f}.png')
+    # plt.close()
 
+#Para el barrido en dYX2, cuento los picos que hay una vez que empieza a decaer el escalón.
+f = 1
+dX2 = 0.1
+Ty2 = 0.21
+dy2 = 0.1
+TY2 = 0.3
+dY2 = 0.1
+epsilon = 0.01
+tb = 530
 
-#%%
-# i_tb = np.where(tiempo>tb)[0][0]
-# 
-# plt.plot(tiempo[i_tb:], X[i_tb:], label='X')
-# plt.plot(tiempo, X, label='X')
-# 
-# picos, caracteristicas = find_peaks(X[i_tb:], height=X[i_tb])
-# altura_picos = caracteristicas['peak_heights']
-# plt.plot(tiempo[i_tb:][picos], altura_picos, 'o')
-# 
-# plt.plot(tiempo_estimulo, estimulo, 'k', label='Estímulo')
+memoria_dYX2 = []
+
+for dYX2 in dYX2_s:
+    tiempo, variables, tiempo_estimulo, estimulo = fpndesf.integra_FPN_estimulo(dYX2, dX2, Ty2, dy2, TY2, dY2, f, tiempo_max=2000, resolucion=10000, ti=0, tf=2000, S_min=0, S_max=0.04, tau=200, ts=30, tb=530, condiciones_iniciales=[0.01, 0.01, 0.01])
+
+    X, y, Y = variables*epsilon
+    i_tb = np.where(tiempo>tb)[0][0]
+    picos, caracteristicas = find_peaks(X[i_tb:], height=X[i_tb])
+    altura_picos = caracteristicas['peak_heights']
+
+    #Memoria: la defino como el número de picos que se cuentan una vez que el estímulo aplicado empieza a decrecer
+    memoria_dYX2.append(len(picos))
+
