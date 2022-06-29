492891d9b3ffc84ad190f89fbb7a1516e6d4dc11
pausanch15 <paulijsanchez@live.com.ar>
2022-06-29 10:35:02 -0300

    Agrego barridos en feedback negativo

--- Tres Nodos/2022_04_25-FPN-con_estimulo-v7.py
+++ Tres Nodos/2022_04_25-FPN-con_estimulo-v7.py
@@ -36,6 +36,7 @@ def encuentra_taus(X):
 
 #%%
 #Empiezo el barrido en los parámetros de feedback positivo y negativo
+#Barrido en feedback positivo
 dYX2 = 0.18 
 dX2 = 0.1 
 Ty2 = 0.21 
@@ -47,7 +48,7 @@ epsilon = 0.01
 
 Nr_s, Tr_s, Tm_s, Tpr_s = [], [], [], []
 
-f_s = np.linspace(0.88, 1.11, 10)
+f_s = np.linspace(0.88, 1.11, 50)
 
 for f in f_s:
     tiempo, variables, tiempo_estimulo, estimulo = fpndesf.integra_FPN_estimulo(dYX2, dX2, Ty2, dy2, TY2, dY2, f, tiempo_max=3000, resolucion=10000, ti=0, tf=2000, S_min=0, S_max=altura_escalon, tau=200, ts=30, tb=1030, condiciones_iniciales=[0.01, 0.01, 0.01])
@@ -66,14 +67,61 @@ for f in f_s:
     plt.grid()
     plt.legend()
     plt.show()
+    plt.savefig(f'resultados/barrido_f_{f}.pdf')
+    plt.close()
 
 #Ploteo los taus en función del valor de f
 plt.figure()
-plt.plot(f_s, Nr_s, 'o', label='Número de Picos en las Reververaciones')
-plt.plot(f_s, Tr_s, 'o', label='Tr')
-plt.plot(f_s, Tm_s, 'o', label='Tm')
-plt.plot(f_s, Tpr_s, 'o', label='Tpr')
+plt.plot(f_s, Nr_s, '-o', label='Nr')
+plt.plot(f_s, Tr_s, '-o', label='Tr')
+plt.plot(f_s, Tm_s, '-o', label='Tm')
+plt.plot(f_s, Tpr_s, '-o', label='Tpr')
 plt.xlabel('f')
+plt.grid()
+plt.legend()
+plt.show()
+
+#Barrido en feedback negativo
+f = 1
+dX2 = 0.1 
+Ty2 = 0.21 
+dy2 = 0.1 
+TY2 = 0.3 
+dY2 = 0.1 
+altura_escalon = 0.1
+epsilon = 0.01
+
+Nr_s, Tr_s, Tm_s, Tpr_s = [], [], [], []
+
+dYX2_s = np.linspace(0.15, 0.22, 50)
+
+for dYX2 in dYX2_s:
+    tiempo, variables, tiempo_estimulo, estimulo = fpndesf.integra_FPN_estimulo(dYX2, dX2, Ty2, dy2, TY2, dY2, f, tiempo_max=3000, resolucion=10000, ti=0, tf=2000, S_min=0, S_max=altura_escalon, tau=200, ts=30, tb=1030, condiciones_iniciales=[0.01, 0.01, 0.01])
+
+    X, y, Y = variables*epsilon
+    Nr, Tr, Tm, Tpr = encuentra_taus(X)
+    Nr_s.append(Nr)
+    Tr_s.append(Tr)
+    Tm_s.append(Tm)
+    Tpr_s.append(Tpr)
+
+    #Plots
+    plt.figure()
+    plt.plot(tiempo, X, label='X')
+    plt.plot(tiempo_estimulo, estimulo, 'k', label='Estímulo')
+    plt.grid()
+    plt.legend()
+    plt.show()
+    plt.savefig(f'resultados/barrido_dYX2_{dYX2}.pdf')
+    plt.close()
+
+#Ploteo los taus en función del valor de f
+plt.figure()
+plt.plot(f_s, Nr_s, '-o', label='Nr')
+plt.plot(f_s, Tr_s, '-o', label='Tr')
+plt.plot(f_s, Tm_s, '-o', label='Tm')
+plt.plot(f_s, Tpr_s, '-o', label='Tpr')
+plt.xlabel('dYX2')
 plt.grid()
 plt.legend()
 plt.show()
