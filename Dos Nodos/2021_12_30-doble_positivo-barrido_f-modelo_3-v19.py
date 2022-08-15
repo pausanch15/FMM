a53d88177e0a202e81787f546272c3b5d60ac39f
pau <paula@sea.dolph>
2022-08-15 09:38:30 -0300

    Agrego graficos

--- Dos Nodos/2021_12_30-doble_positivo-barrido_f-modelo_3-v19.py
+++ Dos Nodos/2021_12_30-doble_positivo-barrido_f-modelo_3-v19.py
@@ -1,5 +1,4 @@
-#Todo el anlálisis para los sistemas cuya memoria se midió bien. En el 2022_07_04-parametros_biestables-modelo_3.csv hay 477 casos para analizar. En el 2022_07_25-parametros_biestables-modelo_3.csv hay 
-#Acá hago el análisis para las corridas que tengo juntas. En v18 están para una a la vez
+#Hacelo mismo que v17 pero acá pretendo sacar los gràficos para la tesis
 
 #Librerías
 import matplotlib.pyplot as plt
@@ -18,7 +17,11 @@ from scipy import interpolate
 import random
 from itertools import combinations
 import latin_hypercube_sampling as lhs
-plt.style.use('seaborn-pastel')
+
+#Cosas de matplotlib para hacer los gráficos
+plt.style.use('ggplot')
+plt.rc("text", usetex=True)
+plt.rc('font', family='serif')
 plt.ion()
 
 #%%
@@ -47,13 +50,6 @@ mem_A = np.concatenate((mem_A_04, mem_A_25))
 mem_B = np.concatenate((mem_B_04, mem_B_25))
 
 #De estas ya sé que muchas fallaron. Las saco, estudio cosas en los que sí salieron bien y posteriormente recalculo la memoria de los sistemas que no salieron bien
-
-#Corrida del 2022_07_04
-# i_fallados = [0, 7, 8, 13, 16, 17, 28, 29, 33, 35, 40, 41, 46, 52, 55, 56, 57, 60, 62, 63, 64, 65, 68, 73, 80, 89, 91, 92, 93, 96, 104, 107, 109, 115, 125, 134, 135, 141, 145, 146, 149, 156, 159, 160, 168, 169, 176, 179, 184, 185, 186, 198, 203, 205, 208, 217, 218, 221, 223, 224, 225, 230, 231, 232, 233, 236, 237, 239, 243, 245, 247, 250, 254, 260, 262, 263, 264, 266, 280, 283, 284, 285, 286, 289, 291, 295, 296, 298, 299, 304, 307, 311, 314, 317, 324, 325, 327, 328, 329, 333, 334, 344, 351, 354, 357, 363, 366, 377, 380, 382, 383, 387, 389, 391, 392, 400, 402, 403, 409, 410, 411, 414, 415, 424, 426, 428, 429, 434, 437, 438, 440, 441, 442, 445, 446, 449, 452, 454, 455, 457, 459, 463, 464, 468, 470, 476, 477, 478, 481, 491, 495, 496, 497, 498, 501, 502, 504, 506, 508, 510, 512, 513, 520, 525, 526, 530, 531, 532, 533, 537, 541, 542, 543, 544, 545, 551, 552, 556, 557, 558, 559, 563, 565, 566, 570, 580, 584, 585, 589, 590, 591, 596, 597, 600, 605, 616, 621, 624, 625, 629, 630, 639, 344, 348, 352, 356, 660, 661, 663, 664, 667, 671, 675, 677, 679, 682, 684, 685, 698, 699, 702, 708, 710, 721, 730, 731, 733, 737, 738, 742, 743, 745, 751, 756, 757]
-
-#Corrida 2022_07_25
-# i_fallados = [0, 4, 5, 6, 7, 15, 17, 18, 24, 25, 26, 29, 30, 31, 34, 37, 39, 45, 49, 54, 55, 56, 60, 62, 64, 67, 68, 69, 71, 73, 75, 77, 79, 80, 81, 86, 89, 90, 96, 100, 101, 103, 105, 108, 116, 120, 121, 122, 125, 127, 131, 133, 138, 140, 146, 147, 148, 150, 151, 153, 154, 158, 167, 168, 171, 173, 174, 182, 183, 187, 189, 192, 200, 204, 205, 208, 217, 218, 220, 224, 226, 227, 228, 229, 231, 235, 236, 239, 240, 241, 247, 249, 251, 252, 255, 256, 257, 258, 261, 262, 263, 266, 267, 271, 273, 276, 277, 283, 285, 286, 288, 293, 294, 295, 297, 301, 306, 307, 308, 311, 314, 316, 318, 319, 322, 324, 325, 327, 329, 330, 331, 332, 333, 335, 339, 348, 351, 357, 359, 363, 364, 365, 373, 376, 381, 382, 384, 385, 389, 394, 397, 399, 400, 408, 410, 411, 413, 417, 421, 430, 432, 438, 439, 441, 447, 450, 452, 456, 457, 461, 464, 468, 469, 473, 476, 477, 479, 480, 486, 488, 491, 495, 497, 499, 505, 511, 512, 516, 517, 518, 519, 521, 522, 523, 525, 526, 533, 534, 537, 543, 545, 548, 554, 558, 559, 561, 563, 564, 567, 569, 572, 573, 576, 578, 579, 587, 589, 590, 597, 598, 600, 601, 602, 603, 605, 606, 617, 619, 620, 622, 624, 626, 627, 629, 630, 631, 634, 637, 643, 648, 649, 651, 652, 656, 659, 662, 663, 664, 667, 668, 674, 676, 677, 682, 683, 694, 695, 696, 699, 700, 701, 702, 704, 705, 708, 711, 713, 715, 717, 719, 722, 724, 726, 727, 728, 729, 733, 734, 738, 745, 746, 747, 748, 752, 758, 759, 762, 764, 766, 767, 769, 771, 773, 777, 780, 782, 784, 787, 791, 793]
-
 #Corridas de 2022_07_04 y 2022_07_25 juntas
 i_fallados_04 = np.array([0, 7, 8, 13, 16, 17, 28, 29, 33, 35, 40, 41, 46, 52, 55, 56, 57, 60, 62, 63, 64, 65, 68, 73, 80, 89, 91, 92, 93, 96, 104, 107, 109, 115, 125, 134, 135, 141, 145, 146, 149, 156, 159, 160, 168, 169, 176, 179, 184, 185, 186, 198, 203, 205, 208, 217, 218, 221, 223, 224, 225, 230, 231, 232, 233, 236, 237, 239, 243, 245, 247, 250, 254, 260, 262, 263, 264, 266, 280, 283, 284, 285, 286, 289, 291, 295, 296, 298, 299, 304, 307, 311, 314, 317, 324, 325, 327, 328, 329, 333, 334, 344, 351, 354, 357, 363, 366, 377, 380, 382, 383, 387, 389, 391, 392, 400, 402, 403, 409, 410, 411, 414, 415, 424, 426, 428, 429, 434, 437, 438, 440, 441, 442, 445, 446, 449, 452, 454, 455, 457, 459, 463, 464, 468, 470, 476, 477, 478, 481, 491, 495, 496, 497, 498, 501, 502, 504, 506, 508, 510, 512, 513, 520, 525, 526, 530, 531, 532, 533, 537, 541, 542, 543, 544, 545, 551, 552, 556, 557, 558, 559, 563, 565, 566, 570, 580, 584, 585, 589, 590, 591, 596, 597, 600, 605, 616, 621, 624, 625, 629, 630, 639, 344, 348, 352, 356, 660, 661, 663, 664, 667, 671, 675, 677, 679, 682, 684, 685, 698, 699, 702, 708, 710, 721, 730, 731, 733, 737, 738, 742, 743, 745, 751, 756, 757])
 
@@ -88,12 +84,15 @@ for i, area in enumerate(areas):
 plt.figure()
 hist, bins = np.histogram(areas_ok, bins='auto')
 logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-# plt.hist(areas_ok, bins=logbins, facecolor='c', density=True, stacked=True, edgecolor = "black")
 plt.hist(areas_ok, bins=logbins, density=True, stacked=True, edgecolor="grey")
 plt.xscale('log')
 plt.yscale('log')
-plt.title('Histograma Áreas')
-plt.grid(zorder=0)
+plt.yticks(fontsize=15, color='black')
+plt.xticks(fontsize=15, color='black')
+plt.xlabel('Tamaño', fontsize=15, color='black')
+plt.ylabel('Cantidad de Áreas', fontsize=15, color='black')
+plt.grid(zorder=-1)
+plt.savefig('Figuras/histareasposloglog.pdf', dpi=300, box_inches='tight')
 
 #%%
 #Gráficos que ven correlación pintando los valores de memoria menores a 0.1 de otro color, y les pongo un alpha para ver la densidad de puntos en el gráfico
@@ -129,43 +128,53 @@ for e, ej in enumerate(ejes):
         del(eje)
 
 #Para la memoria en A
-fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+fig, axs = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(10, 5))
 for i, ax in enumerate(axs.flatten()):
-    ax.plot(ejes_x_A_may[i], mem_A_may, '.c', alpha=0.3)
-    ax.plot(ejes_x_A_men[i], mem_A_men, '.r', alpha=0.3)
-    ax.set_xlabel(labels_x[i])
-    ax.set_ylabel('Memoria en A')
+    ax.plot(ejes_x_A_may[i], mem_A_may, '.', label='Mayores a 0.1')
+    ax.plot(ejes_x_A_men[i], mem_A_men, '.', label='Menores a 0.1')
+    ax.set_xlabel(labels_x[i], fontsize=15, color='black')
+    ax.set_ylabel('Memoria A', fontsize=15, color='black')
+    ax.tick_params(labelsize=15, color='black', labelcolor='black')
     ax.grid()
+    ax.legend()
+plt.savefig('Figuras/corrmemApos.pdf', dpi=300, box_inches='tight')
 
 #Para la memoria en B
-fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+fig, axs = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(10, 5))
 for i, ax in enumerate(axs.flatten()):
-    ax.plot(ejes_x_B_may[i], mem_B_may, '.c', alpha=0.3)
-    ax.plot(ejes_x_B_men[i], mem_B_men, '.r', alpha=0.3)
-    ax.set_xlabel(labels_x[i])
-    ax.set_ylabel('Memoria en B')
+    ax.plot(ejes_x_B_may[i], mem_B_may, '.', label='Mayores a 0.1')
+    ax.plot(ejes_x_B_men[i], mem_B_men, '.', label='Menores a 0.1')
+    ax.set_xlabel(labels_x[i], fontsize=15, color='black')
+    ax.set_ylabel('Memoria B', fontsize=15, color='black')
+    ax.tick_params(labelsize=15, color='black', labelcolor='black')
     ax.grid()
-
-plt.show()
+    ax.legend()
+plt.savefig('Figuras/corrmemBpos.pdf', dpi=300, box_inches='tight')
 
 #Figura de correlación entre las memorias mayores de ambas variables y los valores de altos on y off
 #Altos On
 plt.figure()
 plt.plot(ejes_x_A_may[-2], mem_A_may, '.', label='A: rSpearman=0.73')
 plt.plot(ejes_x_B_may[-2], mem_B_may, '.', label='B: rSpearman=0.41')
-plt.xlabel(labels_x[-2])
-plt.ylabel('Memoria')
+plt.xlabel(labels_x[-2], color='black')
+plt.ylabel('Memoria', color='black')
+plt.yticks(fontsize=15, color='black')
+plt.xticks(fontsize=15, color='black')
 plt.legend()
 plt.grid()
+plt.savefig('Figuras/altosonpos.pdf', dpi=300, box_inches='tight')
 
 #Altos Off
 plt.figure()
 plt.plot(ejes_x_A_may[-1], mem_A_may, '.', label='A: rSpearman=0.86')
 plt.plot(ejes_x_B_may[-1], mem_B_may, '.', label='B: rSpearman=0.67')
-plt.xlabel(labels_x[-1])
-plt.ylabel('Memoria')
+plt.xlabel(labels_x[-1], color='black')
+plt.ylabel('Memoria', color='black')
+plt.yticks(fontsize=15, color='black')
+plt.xticks(fontsize=15, color='black')
 plt.legend()
 plt.grid()
+plt.savefig('Figuras/altosoffpos.pdf', dpi=300, box_inches='tight')
 
 #%%
 #Calculo correlación ignorando los casos de memoria 0. Uso solo el coeficiente de Spearman porque tiene más sentido por lo que contó Fede
